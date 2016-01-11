8ef26f8610285e08d79de9b58a559ae5345499cc
paco <franmenendezpouso@gmail.com>
2016-01-11 11:12:58 +0100

    correccion del metodopara mostrar datos con porcentajes

--- Usuario.java
+++ Usuario.java
@@ -12,36 +12,45 @@ public class Usuario
     private float carbohidratos;
     private float grasas;
     private float caloriasTotales;
-    
-    
+
     public Usuario(String nombreDeUsuario){
         nombreCompleto = nombreDeUsuario;
         proteinas =0;
         carbohidratos = 0;
         grasas = 0;
         caloriasTotales = 0;
-               
-    }
-    
-    public void comer(Alimento alimento, float gramos){
-         proteinas = proteinas +(alimento.getProteinas()/100* gramos);
-        carbohidratos = proteinas +(alimento.getCarbohidratos()/100* gramos);
-         grasas = proteinas +(alimento.getGrasas()/100* gramos);
-        caloriasTotales = proteinas +(alimento.getCaloriasTotales()/100* gramos);
-            
-         //gramos = alimento.getProteinas() + alimento.getCarbohidratos()+ alimento.getGrasas()+ alimento.getCaloriasTotales();
-            
-    }
-    
-    public void mostrarDatos(){
-        String nombreAlimento = "null";
-        Alimento alimento = new Alimento(nombreAlimento,proteinas,carbohidratos,grasas);
-        System.out.println("Nombre:    "+nombreCompleto);
-        System.out.println("Gramos de proteinas ingeridas:    "+alimento.getProteinas());
-        System.out.println("Gramos de carbohidratos ingeridos:    "+alimento.getCarbohidratos());
-        System.out.println("Gramos de grasas ingeridas:    "+alimento.getGrasas());
-        System.out.println("Calorias totales ingeridas:    "+alimento.getCaloriasTotales());
+
     }
 
-    
+    public void comer(Alimento alimento, float gramos){
+        proteinas = proteinas +(alimento.getProteinas()/100* gramos);
+        carbohidratos = proteinas +(alimento.getCarbohidratos()/100* gramos);
+        grasas = proteinas +(alimento.getGrasas()/100* gramos);
+        caloriasTotales = proteinas +(alimento.getCaloriasTotales()/100* gramos);
+
+        //gramos = alimento.getProteinas() + alimento.getCarbohidratos()+ alimento.getGrasas()+ alimento.getCaloriasTotales();
+
+    }
+
+    public void mostrarDatos(){
+        float totalNutrientes = (proteinas + grasas + 			carbohidratos) / 100;
+       		String datosProteinas = "Gramos totales de proteinas ingeridos:     " + proteinas;
+        	String datosCarbohidratos = "Gramos totales de carbohidratos ingeridos: " + 			carbohidratos;
+        	String datosGrasas = "Gramos totales de grasas ingeridos:        " + grasas;
+        	if (proteinas > 0) {
+            	datosProteinas = datosProteinas + " (" + proteinas / totalNutrientes + "%)";
+        	}
+        	if (carbohidratos > 0) {
+            	datosCarbohidratos = datosCarbohidratos + " (" + carbohidratos / totalNutrientes + "%		)";
+        	}
+        	if (grasas > 0) {
+            	datosGrasas = datosGrasas + " (" + grasas / totalNutrientes + "%)";
+        	}
+        	System.out.println("Nombre:                                    " + nombreCompleto);
+        	System.out.println(datosProteinas);    
+        	System.out.println(datosCarbohidratos);
+        	System.out.println(datosGrasas);
+        	System.out.println("Calorias totales ingeridas:                " + caloriasTotales);	
+    }
+
 }
