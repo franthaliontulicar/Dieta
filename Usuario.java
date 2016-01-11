984c7028514673c172804de2dca0937da72080f4
paco <franmenendezpouso@gmail.com>
2016-01-11 12:31:38 +0100

    realizado metodo para comparar consumo de calorias entre 2 usuarios

--- Usuario.java
+++ Usuario.java
@@ -34,23 +34,47 @@ public class Usuario
 
     public void mostrarDatos(){
         float totalNutrientes = (proteinas + grasas + 			carbohidratos) / 100;
-       		String datosProteinas = "Gramos totales de proteinas ingeridos:     " + proteinas;
-        	String datosCarbohidratos = "Gramos totales de carbohidratos ingeridos: " + 			carbohidratos;
-        	String datosGrasas = "Gramos totales de grasas ingeridos:        " + grasas;
-        	if (proteinas > 0) {
-            	datosProteinas = datosProteinas + " (" + proteinas / totalNutrientes + "%)";
-        	}
-        	if (carbohidratos > 0) {
-            	datosCarbohidratos = datosCarbohidratos + " (" + carbohidratos / totalNutrientes + "%		)";
-        	}
-        	if (grasas > 0) {
-            	datosGrasas = datosGrasas + " (" + grasas / totalNutrientes + "%)";
-        	}
-        	System.out.println("Nombre:                                    " + nombreCompleto);
-        	System.out.println(datosProteinas);    
-        	System.out.println(datosCarbohidratos);
-        	System.out.println(datosGrasas);
-        	System.out.println("Calorias totales ingeridas:                " + caloriasTotales);	
+        String datosProteinas = "Gramos totales de proteinas ingeridos:     " + proteinas;
+        String datosCarbohidratos = "Gramos totales de carbohidratos ingeridos: " + 			carbohidratos;
+        String datosGrasas = "Gramos totales de grasas ingeridos:        " + grasas;
+        if (proteinas > 0) {
+            datosProteinas = datosProteinas + " (" + proteinas / totalNutrientes + "%)";
+        }
+        if (carbohidratos > 0) {
+            datosCarbohidratos = datosCarbohidratos + " (" + carbohidratos / totalNutrientes + "%		)";
+        }
+        if (grasas > 0) {
+            datosGrasas = datosGrasas + " (" + grasas / totalNutrientes + "%)";
+        }
+        System.out.println("Nombre:                                    " + nombreCompleto);
+        System.out.println(datosProteinas);    
+        System.out.println(datosCarbohidratos);
+        System.out.println(datosGrasas);
+        System.out.println("Calorias totales ingeridas:                " + caloriasTotales);	
     }
 
+    public String getNombre(){
+        return nombreCompleto;
+
+    }
+
+    public float getCaloriasTotalesIngeridas(){
+        return caloriasTotales;
+
+    }
+
+    public void comparar(Usuario usuario2){
+        if(getCaloriasTotalesIngeridas() > usuario2.getCaloriasTotalesIngeridas()){
+            System.out.println(getNombre()+" ha consumido mas calorias que "+usuario2.getNombre()+" ("+getCaloriasTotalesIngeridas()+" frente a "+usuario2.getCaloriasTotalesIngeridas()+")");
+        }
+        else{
+            if(getCaloriasTotalesIngeridas() == usuario2.getCaloriasTotalesIngeridas()){
+                System.out.println(getNombre()+" y  "+usuario2.getNombre()+" han consumido las mismas calorias ("+getCaloriasTotalesIngeridas()+")");
+            }
+            else{
+                System.out.println(usuario2.getNombre()+" ha consumido mas calorias que "+getNombre()+" ("+usuario2.getCaloriasTotalesIngeridas()+" frente a "+getCaloriasTotalesIngeridas()+")");
+            }
+        }
+
+    }
 }
